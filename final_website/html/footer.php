235c0372f6728fe2196a331b1264e1da917f28b3
Weegee5859 <weegee5859@gmail.com>
2021-03-08 09:10:12 -0500

    php / universal header+footer
    added header.php
    added footer.php
    about_us.html is now about.php
    
    -about.php-
    php includes for header and footer added to about.php
    updated steps icon
    
    -header.php
    navigation bar added
    
    -footer.php
    example footer elements added
    
    notes: if you need to add the header or footer to any page just use php include.

---
+++ final_website/html/footer.php
@@ -0,0 +1,21 @@
+<footer>
+	<div id="footer_container">
+		<ul id="footer_list"></ul>
+			<ul id="footer_item">
+				<h2>About Us</h2>
+				<a href="#">link1</a>
+			</ul>
+			<ul id="footer_item">
+				<h2>Social Media</h2>
+				<a href="#">Instagram</a>
+				<a href="#">Facebook</a>
+				<a href="#">Twitter</a>
+			</ul>
+			<ul id="footer_item">
+				<h2>Contact Us</h2>
+				<a href="#">407-123-4567</a>
+				<a href="#">vacayhouses@gmail.com</a>
+			</ul>
+		</ul>
+	</div>
+</footer>
