4880a528cc3cf9d2ddf13a085af3b6a6cd4bd33d
Weegee5859 <weegee5859@gmail.com>
2021-03-11 12:31:19 -0500

    css / steps
    css
    updated css naming conventions
    udpated steps layout and imgs
    testing color palettes suggested in concepts

--- final_website/html/header.php
+++ final_website/html/header.php
@@ -1,8 +1,8 @@
 <header>
 	<ul id="nav_list">
-		<li id="nav_item"><a href="home.php">Home</a><div id="line"></div></li>
-		<li id="nav_item"><a href="about.php">About Us</a><div id="line"></div></li>
-		<li id="nav_item"><a href="contact.php">Contact Us</a><div id="line"></div></li>
-		<li id="nav_item"><a href="gallery.php">Gallery</a><div id="line"></div></li>
+		<li id="nav_item"><a href="home.php">Home</a><div id="nav_line"></div></li>
+		<li id="nav_item"><a href="about.php">About Us</a><div id="nav_line"></div></li>
+		<li id="nav_item"><a href="contact.php">Contact Us</a><div id="nav_line"></div></li>
+		<li id="nav_item"><a href="gallery.php">Gallery</a><div id="nav_line"></div></li>
 	</ul>
 </header>
