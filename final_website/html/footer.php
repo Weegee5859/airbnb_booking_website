86288c582ce17981848f9069879f5deb6c26cad9
Weegee5859 <weegee5859@gmail.com>
2021-03-11 09:11:40 -0500

    paragraph, footer updated, steps img
    -html
    updated paragraph text and layout
    footer updated and fixed sizing
    
    -css
    id #paragraph-text removed
    edited img sizes

--- final_website/html/footer.php
+++ final_website/html/footer.php
@@ -1,21 +1,21 @@
 <footer>
 	<div id="footer_container">
-		<ul id="footer_list"></ul>
-			<ul id="footer_item">
+		<ul id="footer_list">
+			<div id="footer_item">
 				<h2>About Us</h2>
 				<a href="#">link1</a>
-			</ul>
-			<ul id="footer_item">
+			</div>
+			<div id="footer_item">
 				<h2>Social Media</h2>
 				<a href="#">Instagram</a>
 				<a href="#">Facebook</a>
 				<a href="#">Twitter</a>
-			</ul>
-			<ul id="footer_item">
+			</div>
+			<div id="footer_item">
 				<h2>Contact Us</h2>
 				<a href="#">407-123-4567</a>
 				<a href="#">vacayhouses@gmail.com</a>
-			</ul>
+			</div>
 		</ul>
 	</div>
 </footer>
