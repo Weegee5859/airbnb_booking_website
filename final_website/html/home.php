8fd005dc4ff2cf660eb53a15805b0b89689db16a
Weegee5859 <weegee5859@gmail.com>
2021-04-07 17:32:31 -0400

    udpated homepage
    -html
    Homepage updated
    added title and motto
    calender updated
    added "get started" button, current not linked to anything but will send you to "contact.php" page.

--- final_website/html/home.php
+++ final_website/html/home.php
@@ -14,83 +14,96 @@ Filename:	home.html
 	<link rel="stylesheet" href="css/homepage.css" />
 </head>
 <body id="home" >
-	<?php include 'header.html';?>
-	<!--will be changed to better pictuer, closer up.-->
-	<div>
-	<p> <!--title for calander.-->
-		<h2 id="calH">Book A Day!</h2>
-	</p>
-	<div id="dates">
-		<!--Calander will update to have JavaScript/PHP-->
-		<div id="prev">&lt; Previous</div>
-		<div id="next">Next &gt;</div>
-		<table id="dateCal">
-			<caption>March</caption>
-			<tr>
-				<th>Sun</th>
-				<th>Mon</th>
-				<th>Tue</th>
-				<th>Wed</th>
-				<th>Thu</th>
-				<th>Fri</th>
-				<th>Sat</th>
-			</tr>
-			<tr>
-				<td></td>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-				<td>4</td>
-				<td>5</td>
-				<td>6</td>
-			</tr>
-			<tr>
-				<td>7</td>
-				<td>8</td>
-				<td>9</td>
-				<td>10</td>
-				<td>11</td>
-				<td>12</td>
-				<td>13</td>
-			</tr>
-			<tr>
-				<td>14</td>
-				<td>15</td>
-				<td>16</td>
-				<td>17</td>
-				<td>18</td>
-				<td>19</td>
-				<td>20</td>
-			</tr>
-			<tr>
-				<td>21</td>
-				<td>22</td>
-				<td>23</td>
-				<td>24</td>
-				<td>25</td>
-				<td>26</td>
-				<td>27</td>
-			</tr>
-			<tr>
-				<td>28</td>
-				<td>29</td>
-				<td>30</td>
-				<td>31</td>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-			</tr>
-			<tr>
-				<td>4</td>
-				<td>5</td>
-				<td>6</td>
-				<td>7</td>
-				<td>8</td>
-				<td>9</td>
-				<td>10</td>
-			</tr>
-		</table>
-	</div>
+	<div id="top_image_container">
+		<?php include 'header.html';?>
+
+		<h1 id="website_title">Name_Of_Website</h1>
+
+		<h2 x="0" y="15" id="website_motto">
+			Where Our <span id="website_motto_contrast">Home</span>, Is Your <span id="website_motto_contrast">Home</span>
+		</h2>
+		<!--will be changed to better pictuer, closer up.-->
+		<!--title for calander.-->
+		<p id="calH">Start Booking your Getaway Now!</p>
+		<div id="dates">
+			<!--Calander will update to have JavaScript/PHP-->
+			<div id="dateCal_container">
+				<table id="dateCal">
+					<caption>March 2021</caption>
+					<tr>
+						<th>Sun</th>
+						<th>Mon</th>
+						<th>Tue</th>
+						<th>Wed</th>
+						<th>Thu</th>
+						<th>Fri</th>
+						<th>Sat</th>
+					</tr>
+					<tr>
+						<td></td>
+						<td>1</td>
+						<td>2</td>
+						<td>3</td>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+					</tr>
+					<tr>
+						<td>7</td>
+						<td>8</td>
+						<td>9</td>
+						<td>10</td>
+						<td>11</td>
+						<td>12</td>
+						<td>13</td>
+					</tr>
+					<tr>
+						<td>14</td>
+						<td>15</td>
+						<td>16</td>
+						<td>17</td>
+						<td>18</td>
+						<td>19</td>
+						<td>20</td>
+					</tr>
+					<tr>
+						<td>21</td>
+						<td>22</td>
+						<td>23</td>
+						<td>24</td>
+						<td>25</td>
+						<td>26</td>
+						<td>27</td>
+					</tr>
+					<tr>
+						<td>28</td>
+						<td>29</td>
+						<td>30</td>
+						<td>31</td>
+						<td>1</td>
+						<td>2</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+						<td>7</td>
+						<td>8</td>
+						<td>9</td>
+						<td>10</td>
+					</tr>
+				</table>
+
+				<div id="prev_next_buttons_container">
+					<div id="prev">&lt; Previous</div>
+					<div id="next">Next &gt;</div>
+				</div>
+			</div>
+		</div>
+
+		<input type="date" id="home_input_date">
+		<button type="button" id="contact_us_button">Get Started!</button>
 	</div>
 	<?php include 'footer.html';?>
 </body>
